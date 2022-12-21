08bab12eba1063df5a0fae67394f1ef2b9662efa
Nick Gregory <nick.gregory@openenterprise.co.uk>
2022-12-21 17:32:39 +0000

    reformat

--- src/utils.ts
+++ src/utils.ts
@@ -3,64 +3,64 @@ import * as fs from 'fs'
 import { Parameter } from 'aws-sdk/clients/cloudformation'
 
 export function isUrl(s: string): boolean {
-  let url
+    let url
 
-  try {
-    url = new URL(s)
-  } catch (_) {
-    return false
-  }
+    try {
+        url = new URL(s)
+    } catch (_) {
+        return false
+    }
 
-  return url.protocol === 'https:'
+    return url.protocol === 'https:'
 }
 
 export function parseTags(s: string): aws.CloudFormation.Tags | undefined {
-  let json
+    let json
 
-  try {
-    json = JSON.parse(s)
-  } catch (_) {}
+    try {
+        json = JSON.parse(s)
+    } catch (_) { }
 
-  return json
+    return json
 }
 
 export function parseARNs(s: string): string[] | undefined {
-  return s?.length > 0 ? s.split(',') : undefined
+    return s?.length > 0 ? s.split(',') : undefined
 }
 
 export function parseString(s: string): string | undefined {
-  return s?.length > 0 ? s : undefined
+    return s?.length > 0 ? s : undefined
 }
 
 export function parseNumber(s: string): number | undefined {
-  return parseInt(s) || undefined
+    return parseInt(s) || undefined
 }
 
 export function parseParameters(parameterOverrides: string): Parameter[] {
-  try {
-    const path = new URL(parameterOverrides)
-    const rawParameters = fs.readFileSync(path, 'utf-8')
+    try {
+        const path = new URL(parameterOverrides)
+        const rawParameters = fs.readFileSync(path, 'utf-8')
 
-    return JSON.parse(rawParameters)
-  } catch (err) {
-    // @ts-expect-error: Object is of type 'unknown'
-    if (err.code !== 'ERR_INVALID_URL') {
-      throw err
+        return JSON.parse(rawParameters)
+    } catch (err) {
+        // @ts-expect-error: Object is of type 'unknown'
+        if (err.code !== 'ERR_INVALID_URL') {
+            throw err
+        }
     }
-  }
 
-  const parameters = new Map<string, string>()
-  parameterOverrides.split(',').forEach(parameter => {
-    const [key, value] = parameter.trim().split('=')
-    let param = parameters.get(key)
-    param = !param ? value : [param, value].join(',')
-    parameters.set(key, param)
-  })
+    const parameters = new Map<string, string>()
+    parameterOverrides.split(',').forEach(parameter => {
+        const [key, value] = parameter.trim().split('=')
+        let param = parameters.get(key)
+        param = !param ? value : [param, value].join(',')
+        parameters.set(key, param)
+    })
 
-  return [...parameters.keys()].map(key => {
-    return {
-      ParameterKey: key,
-      ParameterValue: parameters.get(key)
-    }
-  })
+    return [...parameters.keys()].map(key => {
+        return {
+            ParameterKey: key,
+            ParameterValue: parameters.get(key)
+        }
+    })
 }
